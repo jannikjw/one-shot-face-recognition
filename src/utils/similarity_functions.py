a0adf0c8c8541a571d495e8f66cd85b07e643d22
Jannik Wiedenhaupt <jayjay1110@hotmail.de>
2022-11-16 11:59:31 -0500

    Added functionality for proper triplet loss. - picking negative examples from mini batch - picking positive examples from whole train set

--- src/utils/similarity_functions.py
+++ src/utils/similarity_functions.py
@@ -1,4 +1,9 @@
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+eps = 1e-8 # an arbitrary small value to be used for numerical stability tricks
 
 
 def min_norm_2(test_value, trained_examples):
@@ -24,3 +29,47 @@ def cos_sim(a, b):
 def cosine_similarity(test_value, trained_examples):
     similarities = [cos_sim(test_value, anchor).item() for anchor in trained_examples]
     return np.argmax(similarities)
+
+def euclidean_distance_matrix(x):
+  """Efficient computation of Euclidean distance matrix
+  Args:
+    x: Input tensor of shape (batch_size, embedding_dim)
+    
+  Returns:
+    Distance matrix of shape (batch_size, batch_size)
+  """
+  # step 1 - compute the dot product
+
+  # shape: (batch_size, batch_size)
+  dot_product = torch.mm(x, x.t())
+
+  # step 2 - extract the squared Euclidean norm from the diagonal
+
+  # shape: (batch_size,)
+  squared_norm = torch.diag(dot_product)
+
+  # step 3 - compute squared Euclidean distances
+
+  # shape: (batch_size, batch_size)
+  distance_matrix = squared_norm.unsqueeze(0) - 2 * dot_product + squared_norm.unsqueeze(1)
+
+  # get rid of negative distances due to numerical instabilities
+  distance_matrix_pos = F.relu(distance_matrix)
+
+  # step 4 - compute the non-squared distances
+  
+  # handle numerical stability
+  # derivative of the square root operation applied to 0 is infinite
+  # we need to handle by setting any 0 to eps
+  mask = (distance_matrix == 0.0).float()
+
+  # use this mask to set indices with a value of 0 to eps
+  distance_matrix_pos += mask * eps
+
+  # now it is safe to get the square root
+  distance_matrix_root = torch.sqrt(distance_matrix_pos)
+
+  # undo the trick for numerical stability
+  distance_matrix *= (1.0 - mask)
+
+  return distance_matrix
